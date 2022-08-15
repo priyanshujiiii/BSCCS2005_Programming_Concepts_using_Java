3a97b9d3d235d03c177eeda607b6d7306acb4885
Priyanshu D Sharma <89120960+priyanshujiiii@users.noreply.github.com>
2022-08-15 21:06:32 +0530

    Update fundamentals_01.java

--- fundamentals_01.java
+++ fundamentals_01.java
@@ -27,3 +27,68 @@ public class Example{
     System.out.println(integers);
   }
 }
+
+// Example 2
+
+package com.thehecklers;
+
+public class Example2{
+  public void report(){
+    // local variable type inference
+    var aString ="This is a String";
+  
+    // Pattern matching
+    if (aString instanceof String){
+      System.out.println("String variable contains: " + aString);
+    }
+    else{
+      System.out.println("Convert variable contents in string:" + aString.toString());
+    }
+  }
+}
+
+//Example 3
+
+package com.thehecklers;
+public class Greeter {
+  private String name = "mysterious stranger";
+  
+  public Greeter(String name){
+    if(name.length()>0){
+      this.name = name;
+    }
+  }
+  
+  public void greet(){
+    System.out.printlm("Hi, " + name + "!");
+  }
+}
+
+// oops java
+
+package com.thehecklers;
+
+public sealed class Rectangle permits Square {
+  private final int length,width;
+  
+  public Rectangle (int length,int width){
+    this.length = length;
+    this.width = width;
+  }
+  
+  public int getLength(){
+    return length;
+  }
+  
+  public int getwidth(){
+    return width;
+  }
+  
+  public int calculateArea(){
+    return length * width;
+  }
+  
+  public int calculatePerimeter(){
+    return (length * width)*2;
+  }
+}
