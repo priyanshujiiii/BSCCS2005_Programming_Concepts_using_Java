9cce37540e99b8dbcdb45c970df734327df18079
Priyanshu D Sharma <89120960+priyanshujiiii@users.noreply.github.com>
2022-08-15 22:05:13 +0530

    Update fundamentals_02.java

--- fundamentals_02.java
+++ fundamentals_02.java
@@ -4,3 +4,22 @@
 // command line tool
 // Part of the java Development kit(JDK)
 
+// WHY IS IT USEFUL?
+// A read , Evaluate, Print loop for java.
+// Great for exploring new language features.
+// Designed to give extremely rapid feedback step.
+// It's like working withe Play-Deh(TH).
+
+//How was it developed
+// 21 years after the relase of java 1.0 there was enough momentum
+// from the community create a core JDK feature at learning java.
+// # JEP-222: https://openjdk.javanet/jeps/222
+
+//foundation concepts.
+//snippet: a unit of input in Jshell
+//Thee kinds of snippets . No need to type closing ";"
+// expression
+// statement
+// declaration
+// non - nested
+// nested
