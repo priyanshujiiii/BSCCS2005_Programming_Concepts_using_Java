0b2bbd3bc15ead8c238c3ca588e7ede0b1b50d70
Priyanshu D Sharma <89120960+priyanshujiiii@users.noreply.github.com>
2022-08-15 19:39:03 +0530

    Create fundamentals_01.java

---
+++ fundamentals_01.java
@@ -0,0 +1,29 @@
+// Language 
+// STRONGLY typed AND FLEXIBLE 
+// object-oriented
+// functional compatiable.
+// incredibly backward compatible.
+
+// Example
+package com.thehecklers;
+
+import java.until.ArrayList;
+import java.until.List;
+
+public class Example{
+  /* Generics help complier and tools work for you
+  Diamond operator <> demonstrates type inference*/
+  private final List<Strings> strings= new ArrayList<>();
+  private final List<Intgers> Intgers = new ArrayList<>();
+  
+  public Example(){
+    strings.add("Hello there!"); 
+    strings.add("why hi"); 
+    intgers.add(1);
+  }
+  
+  public void report(){
+    System.out.println(strings);
+    System.out.println(integers);
+  }
+}
