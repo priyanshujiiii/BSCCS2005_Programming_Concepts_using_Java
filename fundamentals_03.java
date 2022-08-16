7a088eeea0d771befb05dfcebe2643b831daca3a
Priyanshu D Sharma <89120960+priyanshujiiii@users.noreply.github.com>
2022-08-16 23:33:45 +0530

    Update fundamentals_03.java

--- fundamentals_03.java
+++ fundamentals_03.java
@@ -3,3 +3,13 @@ public class App{
     System.out.println("Hello world");
   }
 }
+
+
+//
+
+public class App{
+  public static void main (String[] args) throws Exception{
+    Person p = new Person("Bill gates");
+    System.out.println("Hello"+ p.getName());
+  }
+}
