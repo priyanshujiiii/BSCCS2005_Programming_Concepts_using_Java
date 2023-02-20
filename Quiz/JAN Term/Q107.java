c18ca8d4d21db2190ab6b0cb1e205c1501d99913
Priyanshu D Sharma <89120960+priyanshujiiii@users.noreply.github.com>
2023-02-20 20:30:21 +0530

    Create Q107.java

---
+++ Quiz/JAN Term/Q107.java
@@ -0,0 +1,10 @@
+public class Fclass{
+    public static void main(String[] args){
+        do{
+            System.out.println("It is hard to find an error in your code");
+            do{
+                System.out.println("When you are looking for it");
+            } while (false);
+        }while(false);
+    }
+}
