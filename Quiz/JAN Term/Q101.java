e9212f513b27972d8cfe1e8b2bdbe14bb4765f8e
Priyanshu D Sharma <89120960+priyanshujiiii@users.noreply.github.com>
2023-02-20 19:25:47 +0530

    Create Q101.java

---
+++ Quiz/JAN Term/Q101.java
@@ -0,0 +1,25 @@
+public class Data{
+    private int d;
+    public Data(){
+        this.d = 10;
+    }
+    public Data(Data t){
+        this.d = t.d;
+    }
+    public void setD(int d){
+        this.d =d;
+    }
+    public int getD(){
+        return d;
+    }
+}
+public class Fclass{
+    public static void main(String[] args){
+        Data dObj1 = new Data();
+        Data dObj2 = dObj1;
+        Data dObj3 = new Data(dObj1);
+        dObj1.setD(20);
+        System.out.printin("dObj2.d : "+ dObj2.getD());
+        System.out.printin("dObj3.d : "+ dObj3.getD());
+    }
+}
