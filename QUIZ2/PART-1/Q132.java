be606e7169f70da0c92d8840a74a6ed6634bb6fe
Priyanshu D Sharma <89120960+priyanshujiiii@users.noreply.github.com>
2023-03-30 18:13:59 +0530

    Q132done

---
+++ QUIZ2/PART-1/Q132.java
@@ -0,0 +1,13 @@
+public class ArrayOperations{
+    public <T extends Comparable> T min (TO) arr) {
+        // code for finding the minimum
+    }
+
+    public <T extends Number> T sum (T[] arr) {
+         // code for finding the sum of elements 
+    }
+
+    public <T> int count (T[] arr) { 
+        // code for finding the count of elements 
+    }
+}
